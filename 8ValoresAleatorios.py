a3ab449ce0089f5b2a9a5499c837a24977a3bced
Felipe Santa Cruz <91490113+Felipewiiu@users.noreply.github.com>
2023-03-23 21:49:19 -0300

    valores aleatórios

--- 8ValoresAleatorios.py
+++ 8ValoresAleatorios.py
@@ -3,20 +3,35 @@ nota = 0
 tamanho = 0
 
 while nota != -1:
-    nota= float(input("Didite uma nota ou -1 para sair do programa: "))
+    nota= int(input("Didite uma nota ou -1 para sair do programa: "))
     if nota != -1:
         grupoNotas.append(nota)
         tamanho += 1
 
 tamanhoArray = len(grupoNotas)
 somaArray = sum(grupoNotas)
-arrayRevet = grupoNotas[::-1]
+arrayRevert = grupoNotas[::-1]
 media = somaArray / tamanho
-
+acimaMedia = sum(1 for nota in grupoNotas if nota > media)
+abaixoDe7 = sum(1 for nota in grupoNotas if nota < 7)
 print("Quantidades de valores lidos: ", tamanhoArray)
-print("Conjunto de valores: ", grupoNotas)
-print("Conjunto de valores na ordem inversa: ", arrayRevet)
+
+
+print("Conjunto de valores informados: ", end="")
+for grupo in grupoNotas:
+    print(grupo, end=" ")
+
+print("\nConjunto de valores na ordem inversa: ")
+for gruporv in arrayRevert:
+    print(gruporv)
+
 print("Soma dos valores: ", somaArray)
 print("Média dos valores: ", media)
+print(f"Valores a cima da média: {acimaMedia}")
+
+print(f"Valores abaixo de 7: {abaixoDe7}")
+print("\nFim do programa")
+
+
 
 
---
+++ 9-comisaoVendas.py
@@ -0,0 +1,3 @@
+produtos = list(("mouse", "teclado", "impressora"))
+print('Lista final!')
+print(produtos)
